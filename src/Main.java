7210f11d358151b8338bfb10c20626cdb3d45dbf
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-21 16:16:01 -0400

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main  {
     public static void main(String [] args){
+        //Printing out hello world to the console
         System.out.println("Hello World");
+        //declaration of a variable
+        String name;
+        String Name;
+        //initialization of a variable
+        name = "Victor";
+        //declaration and initialization of a variable in the same line
+        String newName = "Brad";
+        //reinitializing a variable
+        name = "Sam";
+        System.out.print(name);
+        System.out.println(" Last Name");
+
     }
 }
